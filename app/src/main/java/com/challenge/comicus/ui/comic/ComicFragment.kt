4a07edcbd71710d0f024cb9c6e9cfeeb7ffe9121
pavel.marin <pavel.marin@abax.no>
2020-09-08 18:29:44 +0200

    navigation extensions and initial UI screens

--- app/src/main/java/com/challenge/comicus/ui/comic/ComicFragment.kt
+++ app/src/main/java/com/challenge/comicus/ui/comic/ComicFragment.kt
@@ -1,7 +1,15 @@
 package com.challenge.comicus.ui.comic
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.challenge.comicus.R
+import com.challenge.comicus.ui.base.ComicViewModelFactory
+import com.challenge.comicus.ui.base.model.ComicUi
+import com.challenge.comicus.utils.extensions.getViewModel
 import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
 /**
  * Created by Pavel on 03/09/2020.
@@ -9,8 +17,35 @@ import dagger.android.support.DaggerFragment
 
 class ComicFragment : DaggerFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    @Inject
+    lateinit var viewModelFactory: ComicViewModelFactory
+
+    private lateinit var currentComic: ComicUi
+
+    private val viewModel: ComicViewModel? by lazy {
+        viewModelFactory.getViewModel(activity)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
+        inflater.inflate(R.layout.fragment_comic, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViewModelListeners()
+    }
+
+    private fun initViewModelListeners() {
+        viewModel?.getLatestComic()?.observe(viewLifecycleOwner) {
+            currentComic = it
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel?.fetchLatestComic()
+    }
 }
--- app/src/main/java/com/challenge/comicus/ui/comic/SplashFragment.kt
+++ app/src/main/java/com/challenge/comicus/ui/comic/SplashFragment.kt
@@ -1,7 +1,13 @@
 package com.challenge.comicus.ui.comic
 
 import android.os.Bundle
+import com.challenge.comicus.R
+import com.challenge.comicus.ui.base.ComicViewModelFactory
+import com.challenge.comicus.utils.extensions.getViewModel
+import com.challenge.comicus.utils.extensions.navigate
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_splash.*
+import javax.inject.Inject
 
 /**
  * Created by Pavel on 07/09/2020.
@@ -9,7 +15,26 @@ import dagger.android.support.DaggerFragment
 
 class SplashFragment : DaggerFragment() {
 
+    @Inject
+    lateinit var viewModelFactory: ComicViewModelFactory
+
+    private val comicViewModel: ComicViewModel? by lazy {
+        viewModelFactory.getViewModel(activity)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initClickListener()
+        fetchLatestComic()
+    }
+
+    private fun fetchLatestComic() {
+        comicViewModel?.fetchLatestComic()
+    }
+
+    private fun initClickListener() {
+        playButton.setOnClickListener {
+            navigate(R.id.action_splashFragment_to_comicFragment)
+        }
     }
 }
