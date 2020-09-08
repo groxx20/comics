b50297ee97590a08c7228dc3ebf542a0c2032f98
pavel.marin <pavel.marin@abax.no>
2020-09-08 18:31:30 +0200

    view model implementation

--- app/src/main/java/com/challenge/comicus/ui/comic/ComicViewModel.kt
+++ app/src/main/java/com/challenge/comicus/ui/comic/ComicViewModel.kt
@@ -1,10 +1,45 @@
 package com.challenge.comicus.ui.comic
 
+import android.util.Log
+import androidx.lifecycle.LiveDataReactiveStreams.fromPublisher
+import com.challenge.comicus.data.interactor.FetchComicUseCase
+import com.challenge.comicus.data.interactor.GetComicUseCase
 import com.challenge.comicus.ui.base.BaseViewModel
+import com.challenge.comicus.ui.base.mapper.ComicUiMapper
+import io.reactivex.rxkotlin.subscribeBy
+import timber.log.Timber
 import javax.inject.Inject
 
 /**
  * Created by Pavel on 03/09/2020.
  **/
-class ComicViewModel @Inject constructor() : BaseViewModel() {
+class ComicViewModel @Inject constructor(
+    private val comicUseCase: GetComicUseCase,
+    private val fetchComicUseCase: FetchComicUseCase,
+    private val comicUiMapper: ComicUiMapper
+) : BaseViewModel() {
+
+    fun getLatestComic() = fromPublisher(
+        comicUseCase.getLatestComic()
+            .map(comicUiMapper::mapFromData)
+    )
+
+    fun getComicByNumber(number: Int) = fromPublisher(
+        comicUseCase.getComicByNumber(number)
+            .map(comicUiMapper::mapFromData)
+    )
+
+    fun fetchLatestComic() = fetchComicUseCase.fetchLatestComic()
+        .subscribeBy(
+            onError = { Timber.d(it.localizedMessage) },
+            onComplete = { clearDisposables() }
+        )
+        .addToDisposables()
+
+    fun fetchComicByNumber(number: Int) = fetchComicUseCase.fetchComicByNumber(number)
+        .subscribeBy(
+            onError = { Timber.d(it.localizedMessage) },
+            onComplete = { clearDisposables() }
+        )
+        .addToDisposables()
 }
