9d6bceb110d4617ee1845072cd8b02dcb6512d77
pavel.marin <pavel.marin@abax.no>
2020-09-09 18:39:58 +0200

    adding simple motion layout scene

--- app/src/main/java/com/challenge/comicus/utils/extensions/GeneralExtensions.kt
+++ app/src/main/java/com/challenge/comicus/utils/extensions/GeneralExtensions.kt
@@ -1,6 +1,7 @@
 package com.challenge.comicus.utils.extensions
 
 import android.net.Uri
+import android.os.Handler
 import com.challenge.comicus.utils.constants.ComicAppConstants.EXPLAIN_URL
 import kotlin.random.Random
 
@@ -14,3 +15,11 @@ fun constructExplanationUrl(comicNum: Int, comicTitle: String): Uri =
     Uri.parse("$EXPLAIN_URL$comicNum:_${formatTitle(comicTitle)}")
 
 fun formatTitle(title: String) = title.replace(" ".toRegex(), "_")
+
+val handler: Handler by lazy { Handler() }
+
+fun delayed(delayMillis: Long, delayedFunc: () -> Unit) {
+    handler.postDelayed({
+        delayedFunc.invoke()
+    }, delayMillis)
+}
